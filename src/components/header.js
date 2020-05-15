ccb87ff9a7a5f79452396204bd9e8678b422d06e
Aylin McGinnis <amcginnis@rsglab.com>
2020-05-15 10:19:48 -0400

    Clean up gatsby template

--- src/components/header.js
+++ src/components/header.js
@@ -3,40 +3,34 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header>
+    <nav>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
         </Link>
-      </h1>
-    </div>
+        <Link
+          to="/"
+        >
+          About
+        </Link>
+        <Link
+          to="/"
+        >
+          Travel
+        </Link>
+        <Link
+          to="/"
+        >
+          Portfolio
+        </Link>
+    </nav>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
