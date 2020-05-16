d1294840af8b6561b44075d20be21fdca1313537
Aylin McGinnis <amcginnis@rsglab.com>
2020-05-16 01:12:56 -0400

    Add intro background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import Grid from '@material-ui/core/Grid';
+
 import Layout from "../components/layout"
 import { ButtonLink } from "../components/Button"
 import SEO from "../components/seo"
@@ -9,21 +11,41 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <section id="intro" className={stylesheet.intro}>
-      <h1>Merhaba, I'm Aylin</h1>
-      <p className="caption">I’m an Atlanta based designer and developer who loves creating clean and minimal designs.</p>
-      <ButtonLink href="/about">About Me</ButtonLink>
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={4}>
+          <h1>Merhaba, I'm Aylin</h1>
+          <p className="caption">I’m an Atlanta based designer and developer who loves creating clean and minimal designs.</p>
+          <ButtonLink href="/about">About Me</ButtonLink>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+        </Grid>
+      </Grid>
     </section>
 
     <section id="travel">
-      <h2>Travel</h2>
-      <p className="caption">A few snapshots of where I’ve been</p>
-      <ButtonLink href="/travel">Learn More</ButtonLink>
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={8}>
+          image
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <h2>Travel</h2>
+          <p className="caption">A few snapshots of where I’ve been</p>
+          <ButtonLink href="/travel">Learn More</ButtonLink>
+        </Grid>
+      </Grid>
     </section>
 
     <section id="portfolio">
-      <h2>Portfolio</h2>
-      <p className="caption">My work in design and development</p>
-      <ButtonLink href="/portfolio">My Work</ButtonLink>
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={4}>
+          <h2>Portfolio</h2>
+          <p className="caption">My work in design and development</p>
+          <ButtonLink href="/portfolio">My Work</ButtonLink>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          image
+        </Grid>
+      </Grid>
     </section>
   </Layout>
 )
