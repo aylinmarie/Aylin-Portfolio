26bb4eb4993db48afcd84a88eab7be6dccddc683
Aylin McGinnis <amcginnis@rsglab.com>
2020-05-16 01:36:43 -0400

    Add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,27 +7,33 @@ import { ButtonLink } from "../components/Button"
 import SEO from "../components/seo"
 import stylesheet from './home.module.less';
 
+import mainPic from '../images/pic_main.jpg';
+import portfolioPic from '../images/portfolio_main.png';
+import travelPic from '../images/travel_main.jpg';
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <section id="intro" className={stylesheet.intro}>
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={4}>
+      <Grid container spacing={10}>
+        <Grid item xs={12} sm={5}>
           <h1>Merhaba, I'm Aylin</h1>
           <p className="caption">I’m an Atlanta based designer and developer who loves creating clean and minimal designs.</p>
           <ButtonLink href="/about">About Me</ButtonLink>
         </Grid>
-        <Grid item xs={12} sm={8}>
+        <Grid item xs={12} sm={7}>
+          <img src={mainPic} alt="Headshot of Aylin" />
         </Grid>
       </Grid>
     </section>
 
     <section id="travel">
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={8}>
-          image
+      <Grid container spacing={10}>
+        <Grid item xs={12} sm={7}>
+          <img src={travelPic} alt="Winslow building in Helsinki, Finland" />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={5}>
           <h2>Travel</h2>
           <p className="caption">A few snapshots of where I’ve been</p>
           <ButtonLink href="/travel">Learn More</ButtonLink>
@@ -36,14 +42,14 @@ const IndexPage = () => (
     </section>
 
     <section id="portfolio">
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={4}>
+      <Grid container spacing={10}>
+        <Grid item xs={12} sm={5}>
           <h2>Portfolio</h2>
           <p className="caption">My work in design and development</p>
           <ButtonLink href="/portfolio">My Work</ButtonLink>
         </Grid>
-        <Grid item xs={12} sm={8}>
-          image
+        <Grid item xs={12} sm={7}>
+          <img src={portfolioPic} alt="Camille Simone logo" />
         </Grid>
       </Grid>
     </section>
