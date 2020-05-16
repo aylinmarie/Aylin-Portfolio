903312bb8f63afc22883b6676b529c73d1f20418
Aylin McGinnis <amcginnis@rsglab.com>
2020-05-15 22:48:03 -0400

    Create button component

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import stylesheet from './Button.module.less';
+
+export const Button = ({children, onClick})=> {
+  return(
+    <button onClick={onClick} className={stylesheet.root}>{children}</button>
+  )
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+};
+
+export const ButtonLink = ({children, href})=> {
+  return(
+    <a href={href} className={stylesheet.root}>{children}</a>
+  )
+}
+
+ButtonLink.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string,
+};
