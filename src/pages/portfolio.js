bb9034b3d5d71809790cce24433b1132f86aa352
Aylin McGinnis <amcginnis@rsglab.com>
2020-05-17 23:48:34 -0400

    Set up portfolio nav

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,11 +3,21 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import stylesheet from './portfolio.module.less'
+
 const Portfolio = () => (
   <Layout>
     <SEO title="Portfolio" />
-    <section>
+    <section className={stylesheet.banner}>
       <h1>Portfolio</h1>
+      <p className="h3">My work</p>
+    </section>
+    <section>
+      <ul className={stylesheet.nav}>
+        <li><a className="h3" href="#all">All</a></li>
+        <li><a className="h3" href="#design">Design</a></li>
+        <li><a className="h3" href="#code">Code</a></li>
+      </ul>
     </section>
   </Layout>
 )
