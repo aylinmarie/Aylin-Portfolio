2c6f5e1932d9d3f1f5349fa5d4ef26fdac4007d2
Aylin McGinnis <amcginnis@rsglab.com>
2020-08-04 22:35:50 -0400

    Update portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -9,14 +9,20 @@ import Grid from '@material-ui/core/Grid';
 import stylesheet from './Portfolio.module.less'
 
 // Images
-import aylin from '../../images/portfolio/aylin_portfolio.png'
+import coco from '../../images/portfolio/coco_portfolio.png'
 import bor from '../../images/portfolio/bor_portfolio.png'
 import oyt from '../../images/portfolio/oyt_portfolio.png'
+import cs from '../../images/portfolio/camille_portfolio.png'
+import atoh from '../../images/portfolio/atoh_portfolio.jpg'
+import sofluencial from '../../images/portfolio/sofluencial_portfolio.png'
 
 const PortfolioItems = [
-  {name:'Aylin Marie', src: aylin, category:['all','design'],},
-  {name:'Book of Rhymes', src: bor, category:['all','code'],},
+  {name:'Coco Interiors', src: coco, category:['all','design','development'],},
+  {name:'A Touch of Heart', src: atoh, category:['all','design'],},
+  {name:'Camille Simone Consulting', src: cs, category:['all','design','development'],},
   {name:'OneYoungTraveler', src: oyt, category:['all','design'],},
+  {name:'Book of Rhymes', src: bor, category:['all','development', 'design'],},
+  {name:'Sofluencial', src: sofluencial, category:['all','design'],},
 ]
 
 const Portfolio = () => { 
@@ -42,7 +48,7 @@ const Portfolio = () => {
         <ul className={stylesheet.nav}>
           <li className="h3" active={(filter === 'all').toString()} onClick={() => setFilter('all')}>All</li>
           <li className="h3" active={(filter === 'design').toString()} onClick={() => setFilter('design')}>Design</li>
-          <li className="h3" active={(filter === 'code').toString()} onClick={() => setFilter('code')}>Code</li>
+          <li className="h3" active={(filter === 'development').toString()} onClick={() => setFilter('development')}>Development</li>
         </ul>
 
         <Grid container className={stylesheet.gallery} spacing={4}>
